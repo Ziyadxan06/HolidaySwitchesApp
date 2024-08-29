1bb54c75287da0f799230a4aa3ad350a586f3e5c
Ziyadxan Dovletov <ziyadxan.dovletov06@gmail.com>
2024-08-29 16:36:00 +0400

    Add data binding setup and configure bottom navigation with NavController in MainActivity

--- app/src/main/java/com/ziyad/switchproject/MainActivity.kt
+++ app/src/main/java/com/ziyad/switchproject/MainActivity.kt
@@ -2,10 +2,39 @@ package com.ziyad.switchproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.ziyad.switchproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNavigationView: BottomNavigationView
+    lateinit var navController: NavController
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        bottomNavigationView = binding.bottomNavigation
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.givingFragment,
+                R.id.respectFragment,
+                R.id.happinessFragment,
+                R.id.kindnessFragment,
+                R.id.optimismFragment
+            )
+        )
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
